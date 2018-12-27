ed2dfbd6d61dd13773dfadfe827cce492d1e887f
Reha-Sattar <rehansattar117@gmail.com>
2018-12-28 00:37:51 +0500

    updated connection file

---
+++ server/dbConnection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(
+  'mongodb://localhost/blood-bucket',
+  { useNewUrlParser: true }
+)
+
+const db = mongoose.connection
+
+db.on('connected', function() {
+  console.log('Connected successfully!')
+})
