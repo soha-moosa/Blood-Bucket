3e857d4673fc15afcaaa8f7e14193d13b8be556b
Reha-Sattar <rehansattar117@gmail.com>
2018-12-27 23:36:11 +0500

    entry point updated with / route and test

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const User = require('./playground/Model')
 const port = 8080
-
+require('./playground/mongoDB')
 app.listen(port, err => {
   if (err) {
     console.log(`Error while starting server: ${err}`)
@@ -10,6 +11,21 @@ app.listen(port, err => {
   console.log(`server started on port: ${port}`)
 })
 
+// user addition
+
 app.get('/', (req, res, next) => {
-  res.send('Welcome to Express || Blood Bucket Application')
+  const user = new User({
+    name: 'Rehan',
+    age: 19
+  })
+
+  user
+    .save()
+    .then(user => {
+      res.send(user)
+    })
+    .catch(err => {
+      console.log('error: ', err)
+      res.send({ status: false })
+    })
 })
