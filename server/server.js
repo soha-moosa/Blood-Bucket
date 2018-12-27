1b3386c4447939b234d5bbd4e2b8893e190ab648
Reha-Sattar <rehansattar117@gmail.com>
2018-12-28 01:01:11 +0500

    Server file updated with Body parser

--- server/server.js
+++ server/server.js
@@ -1,31 +1,22 @@
 const express = require('express')
 const app = express()
-const User = require('./playground/Model')
+const bodyParser = require('body-parser')
+const donorRoutes = require('./donor/donor-routes')
+const patientRoutes = require('./patient/patient-routes')
+require('./dbConnection.js')
+
+// parsing the request data
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/donor', donorRoutes)
+app.use('/patient', patientRoutes)
+
 const port = 8080
-require('./playground/mongoDB')
 app.listen(port, err => {
   if (err) {
     console.log(`Error while starting server: ${err}`)
     return
   }
   console.log(`server started on port: ${port}`)
-})
-
-// user addition
-
-app.get('/', (req, res, next) => {
-  const user = new User({
-    name: 'Rehan',
-    age: 19
-  })
-
-  user
-    .save()
-    .then(user => {
-      res.send(user)
-    })
-    .catch(err => {
-      console.log('error: ', err)
-      res.send({ status: false })
-    })
 })
