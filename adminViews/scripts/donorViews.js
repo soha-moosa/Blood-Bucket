e79dade61141974ca026ba7d91c7cc0eade3f248
Reha-Sattar <rehansattar117@gmail.com>
2018-12-30 02:59:21 +0500

    Search by bloodGroup implimented

--- adminViews/scripts/donorViews.js
+++ adminViews/scripts/donorViews.js
@@ -1,9 +1,11 @@
+let allDonors = []
 function fetchAllDonors() {
   fetch('http://localhost:8080/donor/all-donors')
     .then(res => res.json())
     .then(data => {
       if (data.status) {
         console.log(data)
+        allDonors = data.donors
         displayAllDonors(data.donors)
       } else {
         document.getElementById('err-message').innerText = 'No donors till now'
@@ -14,11 +16,13 @@ function fetchAllDonors() {
 
 function displayAllDonors(donors) {
   let tableBody = document.getElementById('donor-table-body')
+
+  tableBody.innerHTML = ''
   donors.forEach(donor => {
     tableBody.innerHTML += `
             <tr>
                 <td>${donor.firstName} ${donor.lastName}</td>
-                <td>${donor.bloodGroup}</td>
+                <td>${donor.bloodGroup.toUpperCase()}</td>
                 <td>${donor.contact}</td>
                 <td>
                     <a class="btn btn-danger text-white"}>View Details</a>
@@ -27,3 +31,29 @@ function displayAllDonors(donors) {
         `
   })
 }
+
+document.getElementById('bloodtype').addEventListener('change', function(e) {
+  if (event.target.value === 'select') {
+    if (allDonors.length > 0) {
+      displayAllDonors(allDonors)
+    } else {
+      document.getElementById('err-message').innerText = 'No donors till now'
+    }
+
+    return
+  }
+  const filteredDonors = allDonors.filter(function(donor, index) {
+    return (
+      donor.bloodGroup.trim().toLowerCase() ===
+      e.target.value.trim().toLowerCase()
+    )
+  })
+
+  if (filteredDonors.length > 0) {
+    displayAllDonors(filteredDonors)
+    document.getElementById('err-message').innerText = ''
+  } else {
+    document.getElementById('donor-table-body').innerHTML = ''
+    document.getElementById('err-message').innerText = 'No donors till now'
+  }
+})
