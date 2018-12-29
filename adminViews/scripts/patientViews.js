54f91d369795cbd57080dc7981675d467e3c6d5d
Reha-Sattar <rehansattar117@gmail.com>
2018-12-30 03:04:11 +0500

    Search by Blood group added to patient

--- adminViews/scripts/patientViews.js
+++ adminViews/scripts/patientViews.js
@@ -1,8 +1,10 @@
+let allPatients = []
 function fetchAllPatients() {
   fetch('http://localhost:8080/patient/all-patients')
     .then(res => res.json())
     .then(data => {
       if (data.status) {
+        allPatients = data.patients
         displayAllpatients(data.patients)
       } else {
         document.getElementById('err-message').innerText =
@@ -14,10 +16,11 @@ function fetchAllPatients() {
 
 function displayAllpatients(patietns) {
   let tableBody = document.getElementById('patient-table-body')
+  tableBody.innerHTML = ''
   patietns.forEach(patient => {
     tableBody.innerHTML += `
               <tr>
-                  <td>${patient.firstName} ${patient.lastName}</td>
+                  <td>${patient.name}</td>
                   <td>${patient.bloodGroup}</td>
                   <td>${patient.contact}</td>
                   <td>
@@ -27,3 +30,29 @@ function displayAllpatients(patietns) {
           `
   })
 }
+
+document.getElementById('bloodtype').addEventListener('change', function(e) {
+  if (event.target.value === 'select') {
+    if (allPatients.length > 0) {
+      displayAllpatients(allPatients)
+    } else {
+      document.getElementById('err-message').innerText = 'No donors till now'
+    }
+
+    return
+  }
+  const filteredPatients = allPatients.filter(function(patient, index) {
+    return (
+      patient.bloodGroup.trim().toLowerCase() ===
+      e.target.value.trim().toLowerCase()
+    )
+  })
+
+  if (filteredPatients.length > 0) {
+    displayAllpatients(filteredPatients)
+    document.getElementById('err-message').innerText = ''
+  } else {
+    document.getElementById('patient-table-body').innerHTML = ''
+    document.getElementById('err-message').innerText = 'No patient till now'
+  }
+})
