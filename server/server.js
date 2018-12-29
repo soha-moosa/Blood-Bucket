1b3f724f1ffeb68d0f0dccbd220016721190ced1
soha-moosa <soha.moosa@yahoo.com>
2018-12-29 23:38:13 +0500

    merging changes

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const donorRoutes = require('./donor/donor-routes')
-const patientRoutes = require('./patient/patient-routes')
-require('./dbConnection.js')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const donorRoutes = require('./donor/donor-routes');
+const patientRoutes = require('./patient/patient-routes');
+require('./dbConnection.js');
 
 // parsing the request data
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/donor', donorRoutes)
-app.use('/patient', patientRoutes)
+app.use('/donor', donorRoutes);
+app.use('/patient', patientRoutes);
 
-const port = 8080
+const port = 8080;
 app.listen(port, err => {
   if (err) {
-    console.log(`Error while starting server: ${err}`)
-    return
+    console.log(`Error while starting server: ${err}`);
+    return;
   }
-  console.log(`server started on port: ${port}`)
-})
+  console.log(`server started on port: ${port}`);
+});
