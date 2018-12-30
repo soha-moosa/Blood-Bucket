ebe42f34c149f11bb784857f166e08cbb461d2f0
Reha-Sattar <rehansattar117@gmail.com>
2018-12-30 17:48:08 +0500

    reading updated

--- adminViews/scripts/masterViews.js
+++ adminViews/scripts/masterViews.js
@@ -3,7 +3,8 @@ function fetchAllPatients() {
     .then(res => res.json())
     .then(data => {
       if (data.status) {
-        console.log(data.patients)
+        console.log(data)
+        displayAllpatients(data.patients)
       } else {
         document.getElementById('err-message').innerText =
           'No patients till now'
@@ -17,7 +18,7 @@ function fetchAllDonors() {
     .then(res => res.json())
     .then(data => {
       if (data.status) {
-        console.log(data.donors)
+        displayAllDonors(data.donors)
       } else {
         document.getElementById('err-message').innerText = 'No donors till now'
       }
@@ -29,3 +30,50 @@ function fetchData() {
   fetchAllDonors()
   fetchAllPatients()
 }
+
+function displayAllpatients(patietns) {
+  let tableBody = document.getElementById('patient-table-body')
+  tableBody.innerHTML = ''
+  patietns.forEach(patient => {
+    tableBody.innerHTML += `
+              <tr>
+                  <td>${patient.name}</td>
+                  <td>${patient.bloodGroup}</td>
+                  <td>${patient.contact}</td>
+                  <td>
+                      <a class="btn btn-danger text-white"}>View Details</a>
+                  </td>
+              </tr>
+          `
+  })
+}
+
+function displayAllDonors(donors) {
+  let tableBody = document.getElementById('donor-table-body')
+
+  tableBody.innerHTML = ''
+  donors.forEach(donor => {
+    tableBody.innerHTML += `
+            <tr>
+                <td>${donor.firstName} ${donor.lastName}</td>
+                <td>${donor.bloodGroup.toUpperCase()}</td>
+                <td>${donor.contact}</td>
+                <td>
+                    <a class="btn btn-danger text-white"}>View Details</a>
+                </td>
+            </tr>
+        `
+  })
+}
+
+document
+  .getElementById('patient-list-btn')
+  .addEventListener('click', function() {
+    document.getElementById('patient-table-body').style.display = 'table'
+    document.getElementById('donor-table-body').style.display = 'none'
+  })
+
+document.getElementById('donor-list-btn').addEventListener('click', function() {
+  document.getElementById('donor-table-body').style.display = 'table'
+  document.getElementById('patient-table-body').style.display = 'none'
+})
