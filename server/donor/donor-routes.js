10d7b995b8ef1a9d3e343a2738ee1be153612a75
Reha-Sattar <rehansattar117@gmail.com>
2019-01-03 21:20:17 +0500

    delete docs middleware added

--- server/donor/donor-routes.js
+++ server/donor/donor-routes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
 const controller = require('./donor-controllers')
-
+const deletePreviouseDocumentsOfDonors = require('../middlewares/donor_Middleware')
 router.post('/register-donor', controller.registerDonor)
-router.get('/all-donors', controller.getAllDonors)
+router.get(
+  '/all-donors',
+  deletePreviouseDocumentsOfDonors,
+  controller.getAllDonors
+)
 router.delete('/delte-donor/:id', controller.deleteDonor)
 
 module.exports = router
