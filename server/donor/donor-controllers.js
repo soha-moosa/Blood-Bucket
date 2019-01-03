8bb45fb6bea9be2233bdb9e64f61862ba8269cad
Reha-Sattar <rehansattar117@gmail.com>
2019-01-03 21:04:12 +0500

    read all controller updated

--- server/donor/donor-controllers.js
+++ server/donor/donor-controllers.js
@@ -1,5 +1,5 @@
 const Donor = require('./donor-model.js')
-
+const firestore = require('../fbConfig')
 function registerDonor(req, res) {
   const newDonor = new Donor(req.body)
   newDonor
@@ -19,20 +19,43 @@ function registerDonor(req, res) {
 }
 
 function getAllDonors(req, res) {
-  Donor.find({})
-    .then(donors =>
-      donors.length > 0
-        ? res.send({
-            status: true,
-            totalDonors: donors.length,
-            donors
-          })
-        : res.send({
-            status: false,
-            message: 'no donors'
-          })
-    )
-    .catch(err => res.send({ status: false, err }))
+  let donorPromises = []
+  // fetching donors from firebase
+  firestore
+    .collection('users')
+    .doc('AVidr3nHFsgDWxudc24C')
+    .collection('donors')
+    .get()
+    .then(querySnapshot => {
+      // creating a local array
+      querySnapshot.forEach(doc => {
+        // setting all documents in a  promised chain array!
+        let newDonor = new Donor(doc.data())
+        donorPromises.push(newDonor.save())
+      })
+
+      Promise.all(donorPromises)
+        .then(flag => {
+          // finding donors locally..!!
+          if (flag) {
+            Donor.find({})
+              .then(donors =>
+                donors.length > 0
+                  ? res.send({
+                      status: true,
+                      totalDonors: donors.length,
+                      donors
+                    })
+                  : res.send({
+                      status: false,
+                      message: 'no donors'
+                    })
+              )
+              .catch(err => res.send({ status: false, err }))
+          }
+        })
+        .catch(err => res.send({ status: false, err }))
+    })
 }
 
 function deleteDonor(req, res) {
