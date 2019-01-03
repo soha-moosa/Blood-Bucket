abf1118f0d33dcd9641454f8687edca8f6033e9b
Reha-Sattar <rehansattar117@gmail.com>
2019-01-03 20:53:04 +0500

    firebase config file in node server

---
+++ server/fbConfig.js
@@ -0,0 +1,20 @@
+const firebase = require('firebase')
+
+const configObject = {
+  apiKey: 'AIzaSyCTDknW5W5NVTs0necuGBmCNFzQkzDpR0w',
+  authDomain: 'blood-bucket.firebaseapp.com',
+  databaseURL: 'https://blood-bucket.firebaseio.com',
+  projectId: 'blood-bucket',
+  storageBucket: 'blood-bucket.appspot.com',
+  messagingSenderId: '629147239690'
+}
+
+var app = firebase.initializeApp(configObject)
+var db = app.firestore()
+
+db.settings({
+  timestampsInSnapshots: true
+})
+
+console.log('connected to firbase with the object => ', db)
+module.exports = db
